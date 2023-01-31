1ecc3afb269e3eadd3a1eedd3041364e6f84a375
shyusu4 <shyusupova4@gmail.com>
2023-01-31 15:05:25 +0500

    Update metadata

--- src/components/CarDetails.jsx
+++ src/components/CarDetails.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const CarDetails = () => {
-  return (
-      <div>
-        <h2>Detail of [Car Name] </h2>
-      </div>
-  );
-}
+const CarDetails = () => (
+  <div>
+    <h2>Detail of [Car Name] </h2>
+  </div>
+);
 
 export default CarDetails;
