946591b7b45ca5d46db0bf85bee3f83163cd2930
riyabulia12 <riyabulia12@gmail.com>
2023-02-01 14:51:06 +0530

    Display info to the view

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import getFavoriteList from "../redux/api";
 
-const Favorite = () => (
+const Favorite = () => {
+  const dispatch = useDispatch();
+  const favoriteCar = useSelector(state => state.favoriteCar);
+
+  useEffect(() => {
+    dispatch(getFavoriteList());
+  }, [dispatch]);
+
+  return(
   <div>
-    <h2>Favorite</h2>
+    <h2>This is Favorite</h2>
+    {
+      Array.isArray(favoriteCar.favoriteCarList) ?
+      favoriteCar.favoriteCarList.map((car, index) => {
+        return <div key={index}>{car.model}</div>
+      }): null
+    }
   </div>
-);
+  )
+};
 
 export default Favorite;
