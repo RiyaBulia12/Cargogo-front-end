075d31a3b436cf349bcfe62571c9c16b6351bf2e
roseokpe1~ <roseokpe@gmail.com>
2023-02-02 05:23:16 -0800

    install ceate thunk

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
+
 import store from './redux/configureStore';
 import { Provider } from 'react-redux'
 
