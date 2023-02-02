caa47365713bb33c2762141debd5debaede6bf3a
roseokpe1~ <roseokpe@gmail.com>
2023-02-02 09:28:21 -0800

    update car reducer

--- src/redux/cars/reducer.js
+++ src/redux/cars/reducer.js
@@ -1,25 +1,21 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-// const baseUrl = 'https://cargogo-backend-production.up.railway.app/api/v1/';
+const baseUrl = 'https://cargogo-backend-production.up.railway.app/api/v1/';
 const GET_CAR_LIST = 'GET_CAR_LIST';
 const initialState = [];
 
-export const getCarList = () => async (dispatch) =>{
-  const carResponse = await fetch(`https://cargogo-backend-production.up.railway.app/api/v1/cars`);
+export const getCarList = () => async (dispatch) => {
+  const carResponse = await fetch(`${baseUrl}cars`);
   const carData = await carResponse.json();
-    console.log(carData)  
-    dispatch({ type: 'GET_CAR_LIST', payload: carData });
-
+  console.log(carData);
+  dispatch({ type: 'GET_CAR_LIST', payload: carData });
 };
 
 const carReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_CAR_LIST:  
-      return action.payload
+    case GET_CAR_LIST:
+      return action.payload;
     default:
       return state;
   }
 };
-
-
 
 export default carReducer;
