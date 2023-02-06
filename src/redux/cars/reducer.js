67c2c540abb7a79ca827895b55fcc7417969c9b7
roseokpe1~ <roseokpe@gmail.com>
2023-02-06 00:15:27 -0800

    add car reducer

--- src/redux/cars/reducer.js
+++ src/redux/cars/reducer.js
@@ -1,5 +1,7 @@
+import axios from "axios";
 const baseUrl = 'https://cargogo-backend-production.up.railway.app/api/v1/';
 const GET_CAR_LIST = 'GET_CAR_LIST';
+const ADD_CAR = 'ADD_CAR';
 const initialState = [];
 
 export const getCarList = () => async (dispatch) => {
@@ -9,10 +11,24 @@ export const getCarList = () => async (dispatch) => {
   dispatch({ type: 'GET_CAR_LIST', payload: carData });
 };
 
+export const addcar = (car) => (dispatch) => {
+  axios
+    .post('https://cargogo-backend-production.up.railway.app/api/v1/cars', car)
+    .then((res) => {
+      dispatch({
+        type: ADD_CAR,
+        payload: res.data,
+      });
+    })
+    .catch((err) => err);
+};
+
 const carReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_CAR_LIST:
       return action.payload;
+    case ADD_CAR:
+      return [...state, action.payload];
     default:
       return state;
   }
