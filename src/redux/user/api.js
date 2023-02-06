77c8041906c5084434bd8ba7e4438689e3190327
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-02-06 10:48:52 +0100

    fix linters

--- src/redux/user/api.js
+++ src/redux/user/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import baseUrl from '../baseUrl';
 
-export const postUser = async (user) => {
-  return await axios.post(`${baseUrl}users`, user);
-};
+export const postUser = async (user) => (
+  axios.post(`${baseUrl}users`, user)
+);
 
-export const getUser = async (userlogin) => {
-    return await axios.post(`${baseUrl}users/login`, userlogin);
-};
+export const getUser = async (userlogin) => (
+     axios.post(`${baseUrl}users/login`, userlogin)
+    );
