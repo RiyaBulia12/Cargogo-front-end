74f1845f517db442175b6b528176b9cc8d22869f
roseokpe1~ <roseokpe@gmail.com>
2023-02-07 12:50:07 -0800

    define axios

--- src/components/AddCar.jsx
+++ src/components/AddCar.jsx
@@ -47,7 +47,7 @@ const Addcar = () => {
   return (
     <form onSubmit={handleChange} className="add-form">
       <h2> ADD A CAR </h2>
-      <input  class="placeholder:italic placeholder:text-slate-400 block bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+      <input 
         type="text"
         required 
         placeholder="Brand"
