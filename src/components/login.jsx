f697c6bb41460a165db90bb827fce4921ac1e6bf
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-02-09 10:58:48 +0100

    Update Login form

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -93,15 +93,15 @@ function Login() {
                         </button>
                       </div>
                       {err && (
-                          <div role="alert">
-                            <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
-                              Error
-                            </div>
-                            <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
-                              <p>{err}</p>
-                            </div>
+                        <div role="alert">
+                          <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                            Error
                           </div>
-                        )}
+                          <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                            <p>{err}</p>
+                          </div>
+                        </div>
+                      )}
                     </form>
                   </div>
                 </div>
