d1f3b04198c280994624bf3c0683eede1f0c9526
riyabulia12 <riyabulia12@gmail.com>
2023-02-09 15:25:53 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import logo from '../assets/cargogo_logo.png';
+import { useLocation } from 'react-router-dom';
 
 export default function Navbar() {
   const [showSidebar, setSidebar] = useState(false);
+  const user = JSON.parse(localStorage.getItem('userInfo'));
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(user ? true : false);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isHomePage = location.pathname === '/';
+  const pos = isHomePage ? 'absolute' : 'relative';
+
+  const logout = () => {
+    localStorage.removeItem('userInfo');
+    setIsUserLoggedIn(false);
+    navigate("/");
+  };
+
 
   return (
-    <nav className="md:px-10 md:py-6 md:mx-8 xs:p-4">
+    <nav className={`md:px-10 md:py-6 md:mx-8 p-4 w-full ${pos}`}>
       <div className="container px-4 flex flex-wrap items-center justify-between mx-auto">
         <Link to="/" className="flex items-center">
           <img src={logo} className="h-7 mr-1 sm:h-12" alt="CarGoGo Logo" />
@@ -22,7 +37,7 @@ export default function Navbar() {
               x
             </button>
           ) : (
-            <button onClick={() => setSidebar(!showSidebar)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden focus:outline-none dark:text-gray-400" aria-controls="navbar-default" aria-expanded="false">
+            <button onClick={() => setSidebar(!showSidebar)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 md:hidden focus:outline-none dark:text-gray-400" aria-controls="navbar-default" aria-expanded="false">
               <span className="sr-only">Open sidebar</span>
               <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
             </button>
@@ -32,23 +47,67 @@ export default function Navbar() {
           }`}
           >
             <ul className="flex flex-col mt-4 left-0 right-[25rem] rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <Link to="/sign-in" className="block py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign in</Link>
-              </li>
-              <li>
-                <Link to="/sign-up" className="block py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign up</Link>
-              </li>
+              {!isUserLoggedIn ? (
+                <>
+                <li>
+                  <Link to="/sign-in" className="block py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign in</Link>
+                </li>
+                <li>
+                  <Link to="/sign-up" className="block py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign up</Link>
+                </li>
+                </>
+              ) : (
+                <>
+                <li>
+                  <Link to="/cars" className="block py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Cars</Link>
+                </li>
+                <li>
+                  <Link to="/add" className="block py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Add Car</Link>
+                </li>
+                <li>
+                  <Link to="/delete" className="block py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Delete Car</Link>
+                </li>
+                <li>
+                  <Link to={`/favorite`} className="block py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Favorites</Link>
+                </li>
+                 <li>
+                  <button onClickCapture={logout} className="block py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</button>
+                </li>
+                </>
+              )}
             </ul>
           </div>
         </>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link to="/sign-in" className="block font-semibold uppercase py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign in</Link>
-            </li>
-            <li>
-              <Link to="/sign-up" className="block font-semibold uppercase py-2 pl-3 pr-4 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign up</Link>
-            </li>
+            {!isUserLoggedIn ? (
+              <>
+              <li className="p-4 md:hover:bg-red-700 text-white">
+                <Link to="/sign-in" className="p-4 block font-semibold uppercase py-2 pl-3 pr-4 text-base hover:bg-gray-100 md:hover:bg-red-700 md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign in</Link>
+              </li>
+              <li className="p-4 md:hover:bg-red-700 text-white">
+                <Link to="/sign-up" className="p-4 block font-semibold uppercase py-2 pl-3 pr-4 text-base hover:bg-gray-100 md:hover:bg-red-700 md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign up</Link>
+              </li>
+              </>
+            ) : (
+              <>
+              <li className="p-4 md:hover:bg-red-700 text-white transform transition-all duration-300">
+                <Link to="/cars" className="p-4 block font-semibold uppercase py-2 pl-3 pr-4 text-base hover:bg-gray-100 md:hover:bg-red-700 md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Cars</Link>
+              </li>
+              <li className="p-4 md:hover:bg-red-700 text-white transform transition-all duration-300">
+                <Link to="/add" className="p-4 block font-semibold uppercase py-2 pl-3 pr-4 text-base hover:bg-gray-100  md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Add Cars</Link>
+              </li>
+              <li className="p-4 md:hover:bg-red-700 text-white transform transition-all duration-300">
+                <Link to="/delete" className="p-4 block font-semibold uppercase py-2 pl-3 pr-4 text-base hover:bg-gray-100  md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Delete Cars</Link>
+              </li>
+              <li className="p-4 md:hover:bg-red-700 text-white transform transition-all duration-300">
+                <Link to={`/favorite`} className="p-4 block font-semibold uppercase py-2 pl-3 pr-4 text-base hover:bg-gray-100  md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Favorites</Link>
+              </li>
+              <li className="p-4 md:hover:bg-red-700 text-white transform transition-all duration-300">
+                <button onClickCapture={logout} className="p-4 block font-semibold uppercase py-2 pl-3 pr-4 text-base hover:bg-gray-100 md:border-0 md:hover:text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</button>
+              </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
