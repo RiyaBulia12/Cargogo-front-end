d839203bf669820adb914d1ab3350c3a92165bd8
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-02-09 10:41:59 +0100

    Style the Usre reg and login

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,95 +1,146 @@
 import React from "react";
 import { useState } from "react";
-import {useDispatch} from "react-redux"
-import { postUser } from './../redux/user/api';
+import { useDispatch } from "react-redux";
+import {useNavigate} from "react-router-dom"
+import { postUser } from "./../redux/user/api";
+import logo from "../assets/cargogo-logo.png";
 
 function Register() {
   const [user, setUser] = useState({
     name: "",
     username: "",
     email: "",
-    password: ""
-  })
+    password: "",
+  });
 
-  const [err, setErr] = useState("")
+  const [err, setErr] = useState([]);
 
   const dispatch = useDispatch();
-  
+  const navigate = useNavigate();
+
   const changeHandler = (event) => {
     setUser({
-      ...user, [event.target.name]: event.target.value,
-    })
-  }
+      ...user,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-  const submitHandler = async(event) => {
+  const submitHandler = async (event) => {
     event.preventDefault();
     const res = await postUser(user);
-    dispatch({ type: 'REGISTER_USER', payload: res.data });
-    if (res.data.error.lenght > 0) {
-      setErr(res.data.error)
-    }   
-  }
+    dispatch({ type: "REGISTER_USER", payload: res.data });
+    if (res.error) {
+      setErr(res.error)
+    } else {
+      setErr(["Registered successfully"])
+      navigate("/sign-in")
+      window.location.reload();
+    }
+  };
 
   return (
-    <div>
-      Register
-      <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          className="Name"
-          placeholder="Name"
-          name="name"
-          onChange={changeHandler}
-        />
+    <section className="h-full gradient-form bg-gray-200 md:h-screen">
+      <div className="container py-12 px-6 h-full">
+        <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+          <div className="xl:w-10/12">
+            <div className="block bg-white shadow-lg rounded-lg">
+              <div className="lg:flex lg:flex-wrap g-0">
+                <div className="lg:w-6/12 px-4 md:px-0">
+                  <div className="md:p-12 md:mx-6">
+                    <div className="text-center">
+                      <img className="mx-auto w-40" src={logo} alt="logo" />
+                      <h4 className="text-xl font-semibold mt-1 mb-12 pb-1">
+                        We are The Lotus Team
+                      </h4>
+                    </div>
+                    <form onSubmit={submitHandler}>
+                      <p className="mb-4">Please login to your account</p>
+                      <div className="mb-4">
+                        <input
+                          type="text"
+                          className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          
+                          placeholder="name"
+                          name="name"
+                          onChange={changeHandler}
+                        />
+                      </div>
 
-        <input
-          type="text"
-          className="Username"
-          placeholder="Username"
-          name="username"
-          onChange={changeHandler}
-        />
-        <input
-          type="email"
-          className="email"
-          placeholder="Email Address"
-          name="email"
-          onChange={changeHandler}
-        />
-        <input
-          type="password"
-          className="password"
-          placeholder="Password"
-          name="password"
-          onChange={changeHandler}
-        />
-        <div className="loginButton">
-          <p>
-            Already have an account?
-            <span>
-              <a href="/sign-in">Sign in</a>
-            </span>
-          </p>
-          <button type="submit">Sign Up</button>
+                      <div className="mb-4">
+                        <input
+                          type="text"
+                          className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          id="exampleFormControlInput1"
+                          placeholder="Username"
+                          name="username"
+                          onChange={changeHandler}
+                        />
+                      </div>
+
+                      <div className="mb-4">
+                        <input
+                          type="email"
+                          className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="Email"
+                          name="email"
+                          onChange={changeHandler}
+                        />
+                      </div>
+                      <div className="mb-4">
+                        <input
+                          type="password"
+                          className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="Password"
+                          name="password"
+                          onChange={changeHandler}
+                        />
+                      </div>
+                      <div className="text-center pt-1 mb-12 pb-1">
+                        <button
+                          className="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3 text-black"
+                          type="submit"
+                          data-mdb-ripple="true"
+                          data-mdb-ripple-color="light"
+                        >
+                          Register
+                        </button>
+                        <div className="text-center text-red-600">
+                          {err}
+                      </div>
+                      </div>
+                      <div className="flex items-center justify-between pb-6">
+                        <p className="mb-0 mr-2">Do have an account?</p>
+                        <button
+                          type="button"
+                          className="inline-block px-6 py-2 border-2 border-red-600 text-red-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                          data-mdb-ripple="true"
+                          data-mdb-ripple-color="light"
+                          onClick={() => {
+                            navigate("/sign-in");
+                          }}
+                        >
+                          Login
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div className="lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none">
+                  <div className="text-white px-4 py-6 md:p-12 md:mx-6">
+                    <h4 className="text-xl font-semibold mb-6">
+                      We are more than just a company
+                    </h4>
+                    <img src="https://images.unsplash.com/photo-1522932467653-e48f79727abf?ixlib=rb-4.0.3&ixid=MnwxM[…]G90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80" 
+                    alt="SideImage"/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="error">
-          {err && err.map((err) => { return err })}
-        </div>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 }
 
 export default Register;
-
-
-
-
-
-
-
-
-
-
-
-
--- src/redux/user/api.js
+++ src/redux/user/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import baseUrl from '../baseUrl';
 
-export const postUser = async (user) => (
-  axios.post(`${baseUrl}users`, user)
-);
+export const postUser = async (user) => {
+  const response = await axios.post(`${baseUrl}users`, user).then((response) => {
+    localStorage.setItem('userInfo', JSON.stringify(response.data.data));
+    return response;
+  });
+  return response.data;
+  };
 
 export const getUser = async (userlogin) => (
   axios.post(`${baseUrl}users/login`, userlogin)
